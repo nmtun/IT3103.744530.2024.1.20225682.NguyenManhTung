83ce9bc8c1f16ceb1448c19b956bd294e6f89f91
nmtun <tungnlpb@gmail.com>
2024-09-25 15:26:23 +0700

    add

---
+++ lab01/FirstDialog.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello! Toi la Tung 20225682. Rat vui khi gap ban");
+        System.exit(0);
+    }
+}
---
+++ lab01/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      System.out.println("Hello! \n");
+      System.out.println("Toi la Nguyen Manh Tung 20225682 \n");
+      System.out.println("Rat vui khi duoc gap ban!!!");
+   } 
+}
